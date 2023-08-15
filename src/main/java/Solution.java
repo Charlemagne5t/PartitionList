ac3644324ebe31933ef6ea5c9d9a70904927a6c9
alek <wait82985@gmail.com>
2023-08-15 14:21:57 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,24 @@
 public class Solution {
     public ListNode partition(ListNode head, int x) {
-        return  null;
+        ListNode leftHead = new ListNode();
+        ListNode leftTail = leftHead;
+        ListNode rightHead = new ListNode();
+        ListNode rightTail = rightHead;
+
+        while (head != null){
+            if(head.val < x){
+                leftTail.next = head;
+                leftTail = leftTail.next;
+            }else{
+                rightTail.next = head;
+                rightTail = rightTail.next;
+            }
+
+            head = head.next;
+        }
+        rightTail.next = null;
+        leftTail.next = rightHead.next;
+
+        return leftHead.next;
     }
 }
